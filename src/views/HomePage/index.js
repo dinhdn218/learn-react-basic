a2c76f5b63c58fbc8f01b8a59870151923144190
dinhdn218 <dinhdn218@gmail.com>
2022-03-14 08:43:33 +0700

    Edit content HomePage

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -5,7 +5,16 @@ class HomePage extends Component {
   render() {
     return (
       <div className="home-page">
-        <h2 className="home-page-title">Học ReactJS cơ bản</h2>
+        <h2 className="home-page-title">
+          Học ReactJS cơ bản. Công nghệ sử dụng:
+        </h2>
+        <p className="home-page-item">
+          - ReactJS (Class Component (95%) + Function Component and Hooks (5%))
+        </p>
+        <p className="home-page-item"> - React Router Dom V6</p>
+        <p className="home-page-item"> - SCSS</p>
+        <p className="home-page-item"> - Redux</p>
+        <p className="home-page-item"> - Toastify lib</p>
       </div>
     );
   }
